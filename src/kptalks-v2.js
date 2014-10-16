4632f216faf891616b27e5180fba00d85cbf1e8f
FuYaoDe <dan826252005@yahoo.com.tw>
2014-10-16 14:01:41 +0800

    更改參數

--- src/kptalks-v2.js
+++ src/kptalks-v2.js
@@ -15,8 +15,8 @@
         popup_color: 'black', // popup font color
         popup_bgcolor: 'beige', // popup background color
         readmore_color: 'brown', // popup font color
-        comein_position: 80, // show kp after scroll more than percent of page height
-        default_text: '歡迎來到高雄巴豆妖部落格，我是小豆~', // the words show in popup before loading done
+        comein_position: 15, // show kp after scroll more than percent of page height
+        default_text: '歡迎來到高雄巴豆妖，我是小豆~', // the words show in popup before loading done
         enter_from: 'left', // options: left, right
         enter_distance: -130 // the distance to window side
 
